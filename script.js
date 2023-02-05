86784bd02e938e2f0676246dac6b9c909bc9eacd
fatmutt <danieljsinger9@gmail.com>
2023-02-04 21:07:50 -0800

    basic etch-a-sketch built

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 
-const container = document.querySelector('container');
 
-const div = document.createElement('div');
+
+/*  div creation in grid */
+for(x=0; x<256; x++) {
+    let board = document.createElement('div');
+    board.className = "bird";
+    document.getElementById('board').appendChild(board);
+    
+    board.addEventListener('mouseover', function() {
+        board.className = 'color';
+            document.getElementsByClassName('color');
+    })
+}
+
+const board = document.querySelector('#board');
+
+/* board.addEventListener('mouseover'), function() {
+    document.getElementByClassName('bird')
+        document.board.style.backgroundColor = 'red';
+} */
+
+/* board.addEventListener('mouseover', e =>{
+    console.log(e)0
+}) */
+
